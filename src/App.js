33731d984b7f788ea62d5af463a310d902f0860f
Tamba Niaty <67123614+Junior0302@users.noreply.github.com>
2023-01-15 12:18:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Junior</h1>
+      <h2>Mon nouveau Projet DashBoard</h2>
+    </div>
+  );
+}
